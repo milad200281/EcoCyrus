216a6bd0f1f080c5d80182ce7bcad1c0f2feb082
Milad Mobini <miladmobini.ca@gmail.com>
2021-04-27 11:14:26 -0400

    news page half way

--- js/script.js
+++ js/script.js
@@ -23,21 +23,53 @@ $(document).ready(function () {
         }, "slow");
     })
     //  #####***** fade pop up feedback ******######
-    setTimeout(()=>{
+    setTimeout(() => {
         $(".popup-feedback").fadeOut(2000);
-    },2000);
+    }, 2000);
 
-    //  #####***** New Owl Carousel ******######
+    //  #####***** News Owl Carousel ******######
     $('.owl-news').owlCarousel({
         items: 1,
         loop: true,
         margin: 10,
         lazyLoad: true,
         autoplay: true,
-        autoplaySpeed:2000,
-        autoplayTimeout:6000,
+        autoplaySpeed: 2000,
+        autoplayTimeout: 6000,
         autoplayHoverPause: true
     });
+
+    //  #####***** News Channel Subscribe ******######
+    
+    $("#subscribe-channel-form").submit(function (e) {
+        //is form valid
+        let valid = true;
+        //RegEx for valid email
+        const valid_email = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        //email input
+        let email = $("#news-letter-email").val();
+        //validating email
+        if (email == '') {
+            valid = false;
+            $("#subs-email-error").text("Email can not be empty");
+        } else if (!email.match(valid_email)) {
+            valid = false;
+            $("#subs-email-error").text("Please enter a valid email");
+        } else {
+            $("#subs-email-error").text("");
+        }
+        if (valid) {
+            this.submit();
+            this.reset();
+        } else {
+            e.preventDefault();
+            $("#subs-email-error").fadeIn(400);
+            setTimeout(() => {
+                $("#subs-email-error").fadeOut(1000);
+            }, 3000);
+        }
+    });
+
+    // set date //
+    const date = document.getElementById("date").innerHTML = new Date().getFullYear();
 })
-// set date //
-const date = document.getElementById("date").innerHTML = new Date().getFullYear();
