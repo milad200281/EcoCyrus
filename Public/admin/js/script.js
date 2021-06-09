4358403aeaea1297a59d7713505f9d9484a987dc
Milad Mobini <miladmobini.ca@gmail.com>
2021-06-09 00:27:40 -0400

    add dashboard page, updated fontawesome

---
+++ Public/admin/js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+    //user profile drop down on click
+    $(".nav-user").click(e=>{
+        $(".user-drop-down").slideToggle(200);
+        $(".alert-box").slideUp(200);
+        $(".sidebar").removeClass('showSideNav');
+    })
+    //notifications drop down on click
+    $(".nav-bell").click(e=>{
+        $(".alert-box").slideToggle(200);
+        $(".user-drop-down").slideUp(200);
+        $(".sidebar").removeClass('showSideNav');
+    })
+// side bar on toggle click
+    $(".nav-toggle").click(e=>{
+        $(".dropdown").slideUp(200);
+        $(".sidebar").toggleClass('showSideNav');
+    })
+    // remove drop downs on click
+    $('.page-content').click(e=>{
+        e.stopPropagation();
+        console.log(e);
+        $(".dropdown").slideUp(200);
+        $(".sidebar").removeClass('showSideNav');
+
+    })
+
+})
